042044e5e406ab811d4af1ed906a1354136cb015
Suhas <murthy.suhas@gmail.com>
2020-04-20 00:51:11 +0530

    reverted

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,20 +3,16 @@ import Switch from "@material-ui/core/Switch";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
 import "./Todo.scss";
-import {todosRef} from "./firebase";
 function Todo(props: any) {
   const { todo } = props;
-  const updateTodo = () => {
-    todosRef.child(todo.id).set({...todo,done:!todo.done})
-  }
   return (
     <div className="Todo">
       <Switch
-        edge="end" checked={todo.done} onChange={updateTodo}
+        edge="end"
         inputProps={{ "aria-labelledby": "switch-list-label-bluetooth" }}
       />
       <p>{todo.task}</p>
-      <IconButton aria-label="delete" onClick={e => todosRef.child(todo.id).remove()}>
+      <IconButton aria-label="delete">
         <DeleteIcon fontSize="large" />
       </IconButton>
     </div>
--- src/TodoForm.tsx
+++ src/TodoForm.tsx
@@ -1,28 +1,10 @@
-import React, { useState } from "react";
-import TextField from "@material-ui/core/TextField";
-import {todosRef} from "./firebase";
+import React from 'react'
+import TextField from '@material-ui/core/TextField'
 function TodoForm() {
-  const [value, setValue] = useState("");
-  const createTodo = (e: React.FormEvent<EventTarget>) => {
-    e.preventDefault();
-    const item = {
-      task: value,
-      done: false,
-    };
-    todosRef.push(item);
-    setValue("");
-  };
-  return (
-    <form onSubmit={createTodo}>
-      <TextField
-        style={{ width: "100%" }}
-        id="outlined-basic"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        label="Add Todo"
-        variant="outlined"
-      />
-    </form>
-  );
+    return (
+        <>
+            <TextField style={{width:"100%"}} id="outlined-basic" label="Add Todo" variant="outlined" />
+        </>
+    )
 }
 export default TodoForm;
