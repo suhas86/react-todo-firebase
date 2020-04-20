f69ead87d7d0305f6aec2bee6873e56686d697d6
Suhas <murthy.suhas@gmail.com>
2020-04-20 17:52:45 +0530

    Firebase added

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Todo from "./Todo";
 import Divider from "@material-ui/core/Divider";
+import {todosRef} from "./firebase";
 function TodoList() {
-  const todoList: any = [
-    { id: "123", task: "First task", done: true },
-    { id: "123", task: "Second task", done: true },
-    { id: "123", task: "Third task", done: true },
-    { id: "123", task: "Fourth task", done: true },
-    { id: "123", task: "Fifth task", done: true },
-    { id: "123", task: "Sixth task", done: true },
-  ];
+  const [todos,setTodos]  = useState<any>([]);
+  useEffect(() => {
+    todosRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          task: items[item].task,
+          done: items[item].done
+        });
+      }
+      setTodos(newState)
+    });
+  },[])
   return (
     <>
-      {todoList.map((todo: any, i: number) => (
-        <>
-          <Todo todo={todo} />
-          {i<todoList.length -1 && <Divider />}
-        </>
+      {todos.map((todo: any, i: number) => (
+        <React.Fragment key={todo.id}>
+          <Todo  todo={todo} />
+          {i<todos.length -1 && <Divider />}
+        </React.Fragment>
       ))}
     </>
   );
---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import "firebase/database"
+const config = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DB,
+    projectId: process.env.REACT_APP_PID,
+    storageBucket: process.env.REACT_APP_SB,
+    messagingSenderId: process.env.REACT_APP_SID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId:process.env.REACT_APP_MID
+};
+firebase.initializeApp(config);
+const databaseRef = firebase.database().ref();
+export const todosRef = databaseRef.child("todos")
+export default firebase;
